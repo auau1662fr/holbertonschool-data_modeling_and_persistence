1ee6ee7478a3c37dea94fcbd3344e2854771e206
auau1662fr <auric.chen.2005@gmail.com>
2026-06-01 11:53:34 +0200

    add 6 7 8

--- sql_join/12-students-with-registrations.sql
+++ sql_join/12-students-with-registrations.sql
@@ -1,7 +1,6 @@
 SELECT students.name AS student_name
 FROM students
 WHERE students.id IN (
-    SELECT student_id
-    FROM enrollments
+    SELECT student_id FROM registrations
 )
 ORDER BY student_name ASC;
